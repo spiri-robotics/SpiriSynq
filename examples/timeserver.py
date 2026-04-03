0cc26ced7f3846da91b4b529534261177af9a953
Alex Davies <traverse.da@gmail.com>
2026-04-03 15:59:18 -0300

    refactor: rename project to SpiriSynq and remove CLI

--- examples/timeserver.py
+++ examples/timeserver.py
@@ -1,12 +1,14 @@
 from dataclasses import dataclass, field
-from SpiriVector.session import SyncableObject, Session
+from SpiriSynq.session import SyncableObject, Session
 import datetime
 import time
 
+#subscribe with sv
 @dataclass
 class TimeData(SyncableObject):
     """Simple server that publishes the current time every second"""
     time: datetime.datetime = field(default_factory=datetime.datetime.now)
+    time_binary: bytes = b''
 
 if __name__ == "__main__":
     session=Session()
@@ -14,4 +16,5 @@ if __name__ == "__main__":
     session.publish_synced_object("time",t)
     while True:
         t.time = datetime.datetime.now()
+        t.time_binary = int(datetime.datetime.now().timestamp()).to_bytes(8)
         time.sleep(1)
