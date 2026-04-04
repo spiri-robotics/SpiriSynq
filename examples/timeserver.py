4e56a305b7772a13c6affc4f04acac6c8318d3f1
Alex Davies <traverse.da@gmail.com>
2026-04-04 10:15:04 -0300

    (no commit message provided)

--- examples/timeserver.py
+++ examples/timeserver.py
@@ -1,14 +1,15 @@
 from dataclasses import dataclass, field
 from SpiriSynq.session import SyncableObject, Session
+from typing import List
 import datetime
 import time
 
-#subscribe with sv
 @dataclass
 class TimeData(SyncableObject):
     """Simple server that publishes the current time every second"""
     time: datetime.datetime = field(default_factory=datetime.datetime.now)
     time_binary: bytes = b''
+    updates: List = field(default_factory=list)
 
 if __name__ == "__main__":
     session=Session()
